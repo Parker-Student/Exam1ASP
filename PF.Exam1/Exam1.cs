7ae2afc00974d578dc9eaf045bf0ce36e313f4c5
Parker Foord <700214020@fvtc.edu>
2019-10-01 12:03:48 -0500

    almost done

--- PF.Exam1/Exam1.cs
+++ PF.Exam1/Exam1.cs
@@ -19,9 +19,12 @@ namespace PF.Exam1
         
         public void RecPerimeter(double height, double width)
         {
+            double perim;
             Double.TryParse(txtWidth.Text, out width);
             Double.TryParse(txtHeight.Text, out height);
-            double perim = (2 * width) + (2 * height);
+            perim = (2 * width) + (2 * height);
+
+            lblPerim.Text = perim.ToString("0.000");
         }
 
         public double RecArea(double height, double width)
@@ -38,21 +41,13 @@ namespace PF.Exam1
             Double.TryParse(txtWidth.Text, out double width);
             Double.TryParse(txtHeight.Text, out double height);
             double temparea = width * height;
+            double a;
+            a = RecArea(height, width);
 
-
-            if(temparea = RecArea() { }
-            
-
-
-
-
-           /* if (Double.TryParse(txtWidth.Text, out double width) && Double.TryParse(txtWidth.Text, out double height))
+            if (Double.TryParse(txtWidth.Text, out  width) && Double.TryParse(txtWidth.Text, out height))
             {
-                double area = width * height;
-                double perim = (2 * width) + (2 * height);
-
-                lblArea.Text = area.ToString("0.000");
-                lblPerim.Text = perim.ToString("0.000");
+                lblArea.Text = a.ToString("0.000");
+                RecPerimeter(height, width);
 
                 txtHeight.Clear();
                 txtWidth.Clear();
@@ -65,7 +60,28 @@ namespace PF.Exam1
                 txtHeight.Clear();
                 txtWidth.Clear();
                 txtWidth.Focus();
-            }*/
+            }
+
+            /* if (Double.TryParse(txtWidth.Text, out double width) && Double.TryParse(txtWidth.Text, out double height))
+             {
+                 double area = width * height;
+                 double perim = (2 * width) + (2 * height);
+
+                 lblArea.Text = area.ToString("0.000");
+                 lblPerim.Text = perim.ToString("0.000");
+
+                 txtHeight.Clear();
+                 txtWidth.Clear();
+
+             }
+             else
+             {
+                 MessageBox.Show("Please Enter a Valid Number", "Error",
+                 MessageBoxButtons.OK, MessageBoxIcon.Error);
+                 txtHeight.Clear();
+                 txtWidth.Clear();
+                 txtWidth.Focus();
+             }*/
         }
 
         private void btnExit_Click(object sender, EventArgs e)
